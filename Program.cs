9786a7af654f43f596b66cb20127b88f3234260f
Szymon Kolarz <72981731+MyshioN@users.noreply.github.com>
2022-11-03 10:50:21 +0100

    Figures areas
    add file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void squareArea(int a)
+        {
+            Console.WriteLine(a * a);
+        }
+
+        static void rectangleArea(int b, int c)
+        {
+            Console.WriteLine(b * c);
+        }
+
+        static void Main(string[] args)
+        {
+            squareArea(4);
+            rectangleArea(2, 4);
+        }
+    }
+}
