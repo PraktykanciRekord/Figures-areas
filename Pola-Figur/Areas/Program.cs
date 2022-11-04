59c60d94b967ff7313d1da057660b896adc8437b
Szymon Kolarz <szymonkolarz@MacBook-Air-Szymon.local>
2022-11-04 09:34:14 +0100

    Minor changes/Folder file name changes

--- Pola-Figur/Areas/Program.cs
+++ Pola-Figur/Areas/Program.cs
@@ -1,111 +1,120 @@
-﻿
-static void squareArea()
+﻿using System;
+
+namespace Areas
 {
-    int a;
-    int pole;
-    Console.WriteLine("Wprowadź długość boku ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.WriteLine("Pole tego kwadratu to: ");
-    pole = (a * 2);
-    Console.WriteLine(pole);
+    class Areas
+    {
+        static void squareArea()
+        {
+            int a;
+            int pole;
+            Console.WriteLine("Wprowadź długość boku ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Pole tego kwadratu to: ");
+            pole = (a * a);
+            Console.WriteLine(pole);
+        }
+
+        static void rectangleArea()
+        {
+            int b;
+            int a;
+            int pole;
+            Console.WriteLine("Wprowadź długość boku a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadź długość boku b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("pole tego prostokątu to: ");
+            pole = (a * b);
+            Console.WriteLine(pole);
+        }
+
+        static void triangleArea()
+        {
+            int a;
+            int h;
+            int pole;
+
+            Console.WriteLine("Wprowadź długość odcinka a ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość wysokości h ");
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("pole tego trójkąta to: ");
+            pole = ((a * h) / 2);
+            Console.WriteLine(pole);
+        }
+
+        static void circleArea()
+        {
+            float r;
+            double pole;
+
+            Console.WriteLine("Wprowadź długość promienia r ");
+            r = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("pole tego koła to: ");
+            pole = (Math.Pow((Math.PI * r), 2));
+            Console.WriteLine(pole);
+        }
+
+        static void trapezeArea()
+        {
+            int a;
+            int b;
+            int h;
+            int pole;
+
+            Console.WriteLine("Wprowadź długość boku a ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość boku b ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość wysokości h ");
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("pole tego trapecu to: ");
+            pole = ((a + b) * h / 2);
+            Console.WriteLine(pole);
+        }
+
+        static void rhombusArea()
+        {
+            int e;
+            int f;
+            int pole;
+
+            Console.WriteLine("Wprowadź długość przeciwległej e ");
+            e = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość przeciwległej f ");
+            f = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("pole tego rombu to: ");
+            pole = (e * f / 2);
+            Console.WriteLine(pole);
+        }
+
+        static void Main(string[] args)
+        {
+            squareArea();
+            //rectangleArea();
+            //triangleArea();
+            //circleArea();
+            //trapezeArea();
+            //rhombusArea();
+        }
+    }
 }
-
-static void rectangleArea()
-{
-    int b;
-    int a;
-    int pole;
-    Console.WriteLine("Wprowadź długość boku a: ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine("Wprowadź długość boku b: ");
-    b = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.WriteLine("pole tego prostokątu to: ");
-    pole = (a + b);
-    Console.WriteLine(pole);
-}
-
-static void triangleArea()
-{
-    int a;
-    int h;
-    int pole;
-
-    Console.WriteLine("Wprowadź długość odcinka a ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość wysokości h ");
-    h = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("pole tego trójkąta to: ");
-    pole = ((a * h) / 2);
-    Console.WriteLine(pole);
-}
-
-static void circleArea()
-{
-    float r;
-    double pole;
-
-    Console.WriteLine("Wprowadź długość promienia r ");
-    r = float.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("pole tego koła to: ");
-    pole = (Math.Pow((Math.PI * r), 2));
-    Console.WriteLine(pole);
-}
-
-static void trapezeArea()
-{
-    int a;
-    int b;
-    int h;
-    int pole;
-
-    Console.WriteLine("Wprowadź długość boku a ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość boku b ");
-    b = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość wysokości h ");
-    h = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("pole tego trapecu to: ");
-    pole = ((a + b) * h / 2);
-    Console.WriteLine(pole);
-}
-
-static void rhombusArea()
-{
-    int e;
-    int f;
-    int pole;
-
-    Console.WriteLine("Wprowadź długość przeciwległej e ");
-    e = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość przeciwległej f ");
-    f = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("pole tego rombu to: ");
-    pole = (e * f / 2);
-    Console.WriteLine(pole);
-}
-
-squareArea();
-//rectangleArea();
-//triangleArea();
-//circleArea();
-//trapezeArea();
-//rhombusArea();
-
--- Pola-Figur/Perimeters/Perimeters.cs
+++ Pola-Figur/Perimeters/Perimeters.cs
@@ -1,117 +1,127 @@
-﻿
-static void squarePerimeter()
+﻿using System;
+
+namespace Perimeters
 {
-    int a;
-    int pole;
-    Console.WriteLine("Wprowadź długość boku ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.WriteLine("Obwód tego kwadratu to : ");
-    pole = (a * 4);
-    Console.WriteLine(pole);
+    class Perimeters
+    {
+
+        static void squarePerimeter()
+        {
+            int a;
+            int pole;
+            Console.WriteLine("Wprowadź długość boku ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Obwód tego kwadratu to : ");
+            pole = (a * 4);
+            Console.WriteLine(pole);
+        }
+
+        static void rectanglePerimeter()
+        {
+            int b;
+            int a;
+            int obwod;
+            Console.WriteLine("Wprowadź długość boku a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadź długość boku b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("obwód tego prostokątu to: ");
+            obwod = ((a * 2) + (b * 2));
+            Console.WriteLine(obwod);
+        }
+
+        static void trianglePerimeter()
+        {
+            int a;
+            int b;
+            int c;
+            int obwód;
+
+            Console.WriteLine("Wprowadź długość boku a ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość boku b ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość boku c ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("obwód tego trójkąta to: ");
+            obwód = (a + b + c);
+            Console.WriteLine(obwód);
+        }
+
+        static void trapezePerimeter()
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            int obwód;
+
+            Console.WriteLine("Wprowadź długość boku a ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość boku b ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość boku c ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Wprowadź długość boku d ");
+            d = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("obwód tego trapezu to: ");
+            obwód = (a + b + c + d);
+            Console.WriteLine(obwód);
+        }
+
+        static void circlePerimeter()
+        {
+            float r;
+            double obw;
+
+            Console.WriteLine("Wprowadź długość promienia r ");
+            r = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("obwód tego koła to: ");
+            obw = ((Math.PI * r) * 2);
+            Console.WriteLine(obw);
+        }
+
+        static void rhombusPerimeter()
+        {
+            int a;
+            int obw;
+
+            Console.WriteLine("Wprowadź długość odcinka a ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("obwód tego rombu to: ");
+            obw = (a * 4);
+            Console.WriteLine(obw);
+        }
+
+        static void Main(string[] args)
+        {
+            squarePerimeter();
+            //rectanglePerimeter();
+            //trianglePerimeter();
+            //trapezePerimeter();
+            ///circlePerimeter();
+            //rhombusPerimeter();  
+
+        }
+    }
 }
-
-static void rectanglePerimeter()
-{
-    int b;
-    int a;
-    int obwod;
-    Console.WriteLine("Wprowadź długość boku a: ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine("Wprowadź długość boku b: ");
-    b = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.WriteLine("obwód tego prostokątu to: ");
-    obwod = ((a * 2) + (b * 2));
-    Console.WriteLine(obwod);
-}
-
-static void trianglePerimeter()
-{
-    int a;
-    int b;
-    int c;
-    int obwód;
-
-    Console.WriteLine("Wprowadź długość boku a ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość boku b ");
-    b = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość boku c ");
-    c = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("obwód tego trójkąta to: ");
-    obwód = (a + b + c);
-    Console.WriteLine(obwód);
-}
-
-static void trapezePerimeter()
-{
-    int a;
-    int b;
-    int c;
-    int d;
-    int obwód;
-
-    Console.WriteLine("Wprowadź długość boku a ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość boku b ");
-    b = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość boku c ");
-    c = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("Wprowadź długość boku d ");
-    d = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("obwód tego trapezu to: ");
-    obwód = (a + b + c + d);
-    Console.WriteLine(obwód);
-}
-
-static void circlePerimeter()
-{
-    float r;
-    double obw;
-
-    Console.WriteLine("Wprowadź długość promienia r ");
-    r = float.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("obwód tego koła to: ");
-    obw = ((Math.PI * r) * 2);
-    Console.WriteLine(obw);
-}
-
-static void rhombusPerimeter()
-{
-    int a; 
-    int obw;
-
-    Console.WriteLine("Wprowadź długość odcinka a ");
-    a = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    Console.WriteLine("obwód tego rombu to: ");
-    obw = (a * 4);
-    Console.WriteLine(obw);
-}
-
-        
-squarePerimeter();
-//rectanglePerimeter();
-//trianglePerimeter();
-//trapezePerimeter();
-///circlePerimeter();
-//rhombusPerimeter();  
-        
