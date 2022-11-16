840814964967e3963eaf79d561a1d80da9fd3ec4
MyshioN <szymonkolarz29@gmail.com>
2022-11-16 11:08:15 +0100

    done

--- PolaFigurTests/PolaFigurTests.cs
+++ PolaFigurTests/PolaFigurTests.cs
@@ -18,18 +18,18 @@ public class PolaFigurTests
 
         Rhombus rh = new Rhombus();
         Assert.Equal(12 ,Rhombus.RhoArea(4, 6));
-        Assert.Equal(12, Rhombus.rhomPer(4));
+        Assert.Equal(16, Rhombus.rhomPer(4));
 
         Trapeze tr = new Trapeze();
-        Assert.Equal(29, Trapeze.TrAr(8, 6, 4));
+        Assert.Equal(28, Trapeze.TrAr(8, 6, 4));
         Assert.Equal(22, Trapeze.TrPe(8, 6, 4, 4));
 
         Triangle tria = new Triangle();
         Assert.Equal(12, Triangle.TriAr(4, 6));
-        Assert.Equal(20, Triangle.TriPer(4, 6, 8));
+        Assert.Equal(18, Triangle.TriPer(4, 6, 8));
 
         Circle cr = new Circle();
-        Assert.Equal(10, Circle.CirAr(4));
-        Assert.Equal(50.24, Circle.CirPer(8));
+        Assert.Equal(157.91367041742973, Circle.CirAr(4));
+        Assert.Equal(50.26548245743669, Circle.CirPer(8));
     }
 }
