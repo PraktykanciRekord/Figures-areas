a2d4bab0b71f4eedb78e245faea60d86adfcac1f
MyshioN <szymonkolarz29@gmail.com>
2022-11-21 08:41:31 +0100

    Added Api

---
+++ PolaFigurApi/Controllers/PolaFigur.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApplication;
+namespace PolaFigurApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PolaFigur : ControllerBase
+{
+    SquareArea sq = new(); 
+
+    [HttpGet]
+    public string Get(string a)
+    {
+        return SquareArea.SquareA(double.Parse(a)).ToString();
+    }
+}
+
